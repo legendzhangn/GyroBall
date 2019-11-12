ef24527592a34b313aa4490dc0bdf86979548ba3
Nan Zhang <legendzhangn@gmail.com>
2019-11-11 19:58:53 -0800

    Add GyroBall android app

---
+++ app/src/main/java/com/goldsequence/gyroball/HelpActivity.java
@@ -0,0 +1,16 @@
+package com.goldsequence.gyroball;
+
+import com.goldsequence.gyroball.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class HelpActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_help); 
+	}
+}
